06c77bf384b433095b403b6d48f66a5bc56b4635
Ismailjabiullah82 <ijabiullah@gmail.comm>
2024-10-28 00:17:52 +0400

    home & play ground & score section added

---
+++ script.js
@@ -0,0 +1,5 @@
+function getRandomElphabet() {
+    const alphabetsString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetsString.split("");
+    console.log(alphabets);
+  }
---
+++ scripts/alpha-class.js
@@ -0,0 +1,8 @@
+function play(){
+    console.log('play start now');
+}
+function getRandomElphabet() {
+    const alphabetsString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetsString.split("");
+    console.log(alphabets);
+  }
